ce974ec597f1ac88a149b4f9752d042512e84194
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-05-22 21:20:02 +0530

    Init

---
+++ Functions/ToSkeleton/app.py
@@ -0,0 +1,60 @@
+from skimage.morphology import skeletonize
+from skimage import data
+import matplotlib.pyplot as plt
+from skimage.util import invert
+import cv2
+import numpy as np
+# Invert the horse image
+# image = invert(data.horse())
+# print(image)
+
+image=cv2.imread('Input.jpg')
+
+# image
+image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
+height=image.shape[0]
+width=image.shape[1]
+Ratio=height/540
+
+
+image=cv2.resize(image,(int(width/Ratio),int(height/Ratio)))
+OgImage=cv2.resize(image,(int(width/Ratio),int(height/Ratio)))
+
+# kernelSizes = [(3, 3), (9, 9), (15, 15)]
+
+# image=cv2.blur(image, (100, 100))
+# image=cv
+OgImage=image
+image=invert(image)
+
+skeleton = skeletonize(image)
+
+skeleton=np.array(skeleton)
+skeleton=cv2.cvtColor(skeleton,cv2.COLOR_RGB2GRAY)
+
+
+skeleton=invert(skeleton)
+image=invert(image)
+# fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(8, 4),
+#                          sharex=True, sharey=True)
+
+# ax = axes.ravel()
+
+
+# ax[0].imshow(OgImage, cmap=plt.cm.gray)
+# ax[0].axis('off')
+# ax[0].set_title('original', fontsize=20)
+
+# ax[1].imshow(skeleton, cmap=plt.cm.gray)
+# ax[1].axis('off')
+# ax[1].set_title('skeleton', fontsize=20)
+
+# # ax[2].imshow(skeleton+OgImage, cmap=plt.cm.gray)
+# # ax[2].axis('off')
+# # ax[2].set_title('skeleton2', fontsize=20)
+
+# fig.tight_layout()
+# plt.show()
+# cv2.imwrite('op.jpg',image)
+cv2.imwrite('Output.jpg',skeleton)
---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Path Finding App
+
+
+### To Setup
+```
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+### To Run 
+```
+python app.py
+```
+
+### Features
+- Convert Drawing to Skeleton Image (Rhys Done)
+- Skeleton Image To Graph 
+- Find Graph all Min Paths 
+- Vectorise Data (.svg)
+- Camera Takes Pic of Graph on Paper and does Calc
+- Draw in App
+- Use CNN to Get Graph (Rhys)
+
+
